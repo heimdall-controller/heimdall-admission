4197f42488534cc97bccb02e65c189dd2a1dd06f
Bryan Keane <bkeane@redhat.com>
2023-04-12 19:49:34 +0100

    HEIMCTR-56: Added simple logic to block requests from non-owner IP

--- cmd/webhook-server/admission_controller.go
+++ cmd/webhook-server/admission_controller.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"errors"
 	"fmt"
@@ -33,7 +34,7 @@ type patchOperation struct {
 
 // admitFunc is a callback for admission controller logic. Given an AdmissionRequest, it returns the sequence of patch
 // operations to be applied in case of success, or the error that will be shown when the operation is rejected.
-type admitFunc func(*v1beta1.AdmissionRequest) ([]patchOperation, error)
+type admitFunc func(*v1beta1.AdmissionRequest, string, string) ([]patchOperation, error)
 
 // isKubeNamespace checks if the given namespace is a Kubernetes-owned namespace.
 func isKubeNamespace(ns string) bool {
@@ -46,20 +47,14 @@ var errs []string
 // request -- delegates the admission control logic to the given admitFunc. The response body is then returned as raw
 // bytes.
 func doServeAdmitFunc(w http.ResponseWriter, r *http.Request, admit admitFunc) ([]byte, error) {
-	logrus.Infof("Processing %s request from %s", r.Method, r.RemoteAddr)
-
-	// parse the request body into a json object
-	var requestJson map[string]interface{}
-	err := json.NewDecoder(r.Body).Decode(&requestJson)
-	if err != nil {
-		errs = append(errs, fmt.Sprintf("could not decode request body: %v", err))
-		return nil, err
-	}
+	logrus.Infof("Processing %s request", r.Method)
 
 	if r.Method != http.MethodPost {
 		w.WriteHeader(http.StatusMethodNotAllowed)
 		return nil, fmt.Errorf("invalid method %s, only POST requests are allowed", r.Method)
 	}
+
+	logrus.Info("method is POST, continuing")
 
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
@@ -68,13 +63,18 @@ func doServeAdmitFunc(w http.ResponseWriter, r *http.Request, admit admitFunc) (
 		return nil, fmt.Errorf("could not read request body: %v", err)
 	}
 
+	logrus.Info("body bytes processed, continuing")
+
 	if contentType := r.Header.Get("Content-Type"); contentType != jsonContentType {
 		errs = append(errs, fmt.Sprintf("unsupported content type %s, only %s is supported", contentType, jsonContentType))
 		w.WriteHeader(http.StatusBadRequest)
 		return nil, fmt.Errorf("unsupported content type %s, only %s is supported", contentType, jsonContentType)
 	}
 
-	// Parse the AdmissionReview request.
+	logrus.Info("content type is json, continuing")
+
+	// Step 2: Parse the AdmissionReview request.
+
 	var admissionReviewReq v1beta1.AdmissionReview
 
 	if _, _, err := universalDeserializer.Decode(body, nil, &admissionReviewReq); err != nil {
@@ -87,54 +87,76 @@ func doServeAdmitFunc(w http.ResponseWriter, r *http.Request, admit admitFunc) (
 		return nil, errors.New("malformed admission review: request is nil")
 	}
 
-	// Construct the AdmissionReview response.
+	logrus.Info("admissionReviewReq processed, continuing")
+	r.Body = ioutil.NopCloser(bytes.NewReader(body))
+
+	// parse the request body into a json object
+	var requestJson map[string]interface{}
+	err = json.NewDecoder(r.Body).Decode(&requestJson)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		logrus.Errorf("Error decoding request body: %v", err)
+		return nil, err
+	}
+	logrus.Info("The request body has been decoded")
+
+	// Convert requestJson["request"].(map[string]interface{})["object"] to unstructured
+	objectJson := requestJson["request"].(map[string]interface{})["object"].(map[string]interface{})
+	unstructuredObject := &unstructured.Unstructured{Object: objectJson}
+	var ownerIP string
+	if unstructuredObject.GetLabels()["app.heimdall.io/owner"] != "" {
+		ownerIP = unstructuredObject.GetLabels()["app.heimdall.io/owner"]
+
+		logrus.Infof("owner label: %s", ownerIP)
+		if ownerIP != strings.Split(r.RemoteAddr, ":")[0] {
+			w.WriteHeader(http.StatusMethodNotAllowed)
+			return nil, fmt.Errorf("owner ip %s does not match request address %s", ownerIP, r.RemoteAddr)
+		} else {
+			logrus.Infof("owner ip %s matches request address %s", ownerIP, r.RemoteAddr)
+		}
+	}
+
+	senderIP := strings.Split(r.RemoteAddr, ":")[0]
+	logrus.Infof("sender ip: %s", senderIP)
+
+	// Step 3: Construct the AdmissionReview response.
+
 	admissionReviewResponse := v1beta1.AdmissionReview{
+		TypeMeta: admissionReviewReq.TypeMeta,
 		Response: &v1beta1.AdmissionResponse{
 			UID: admissionReviewReq.Request.UID,
 		},
 	}
 
-	remoteAddress := strings.Split(r.RemoteAddr, ":")
-	requesterIP, _ := remoteAddress[0], remoteAddress[1]
-
-	// Convert requestJson["request"].(map[string]interface{})["object"] to unstructured
-	objectJson := requestJson["request"].(map[string]interface{})["object"].(map[string]interface{})
-	unstructuredObject := &unstructured.Unstructured{Object: objectJson}
+	logrus.Info("admissionReviewResponse constructed, continuing")
 
 	var patchOps []patchOperation
-	patchOps, err = admit(admissionReviewReq.Request)
-	if err != nil {
-		admissionReviewResponse.Response.Allowed = false
-		admissionReviewResponse.Response.Result = &metav1.Status{
-			Message: err.Error(),
-		}
-	}
+	// Apply the admit() function only for non-Kubernetes namespaces. For objects in Kubernetes namespaces, return
+	// an empty set of patch operations.
+	if !isKubeNamespace(admissionReviewReq.Request.Namespace) {
+		patchOps, err = admit(admissionReviewReq.Request, senderIP, ownerIP)
 
-	if unstructuredObject.GetLabels()["app.heimdall.io/owner"] != "" {
-		ownerIP := unstructuredObject.GetLabels()["app.heimdall.io/owner"]
+		logrus.Info("admit function called, continuing")
 
-		if ownerIP != requesterIP {
-			logrus.Warnf("owner ip %s does not match request address %s, denying the request", ownerIP, r.RemoteAddr)
+		if err != nil {
 			admissionReviewResponse.Response.Allowed = false
 			admissionReviewResponse.Response.Result = &metav1.Status{
-				Message: fmt.Sprintf("owner ip %s does not match request address %s, denying the request", ownerIP, r.RemoteAddr),
+				Message: err.Error(),
 			}
+
+			logrus.Info("err is not nil, so set response.Allowed to false, continuing")
+
 		} else {
 			// Otherwise, encode the patch operations to JSON and return a positive response.
 			patchBytes, err := json.Marshal(patchOps)
 			if err != nil {
-				errs = append(errs, fmt.Sprintf("could not marshal JSON patch: %v", err))
 				w.WriteHeader(http.StatusInternalServerError)
 				return nil, fmt.Errorf("could not marshal JSON patch: %v", err)
 			}
 			admissionReviewResponse.Response.Allowed = true
 			admissionReviewResponse.Response.Patch = patchBytes
 
-			admissionReviewResponse.Response.Allowed = true
-			admissionReviewResponse.Response.Result = &metav1.Status{
-				Message: fmt.Sprintf("owner ip %s matches request address %s, admitting the request", ownerIP, r.RemoteAddr),
-			}
-			logrus.Infof("owner ip %s matches request address %s, admitting the request", ownerIP, r.RemoteAddr)
+			logrus.Info("err is nil, so set response.Allowed to true, continuing")
 		}
 	}
 
@@ -144,6 +166,8 @@ func doServeAdmitFunc(w http.ResponseWriter, r *http.Request, admit admitFunc) (
 		errs = append(errs, fmt.Sprintf("marshaling response: %v", err))
 		return nil, fmt.Errorf("marshaling response: %v", err)
 	}
+
+	logrus.Info("response marshalled, returning bytes")
 	return bytes, nil
 }
 
