a6255133f8c5770b5f3a7b61ee4f669de64855d3
Bryan Keane <bkeane@redhat.com>
2023-04-12 14:28:18 +0100

    HEIMCTR-55: Added check for requester IP matching owner IP

--- cmd/webhook-server/admission_controller.go
+++ cmd/webhook-server/admission_controller.go
@@ -1,17 +1,34 @@
+/*
+Copyright (c) 2019 StackRox Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package main
 
 import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"github.com/sirupsen/logrus"
 	"io/ioutil"
-	"log"
-	"net/http"
-
-	admission "k8s.io/api/admission/v1"
+	"k8s.io/api/admission/v1beta1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/serializer"
+	"log"
+	"net/http"
 )
 
 const (
@@ -31,7 +48,7 @@ type patchOperation struct {
 
 // admitFunc is a callback for admission controller logic. Given an AdmissionRequest, it returns the sequence of patch
 // operations to be applied in case of success, or the error that will be shown when the operation is rejected.
-type admitFunc func(*admission.AdmissionRequest) ([]patchOperation, error)
+type admitFunc func(*v1beta1.AdmissionRequest) ([]patchOperation, error)
 
 // isKubeNamespace checks if the given namespace is a Kubernetes-owned namespace.
 func isKubeNamespace(ns string) bool {
@@ -43,6 +60,41 @@ func isKubeNamespace(ns string) bool {
 // bytes.
 func doServeAdmitFunc(w http.ResponseWriter, r *http.Request, admit admitFunc) ([]byte, error) {
 	// Step 1: Request validation. Only handle POST requests with a body and json content type.
+
+	//TODO process the request
+	// 1. get the request body
+	// get a copy of the resource in question
+	// check if the string "app.heimdall.io" is in the key of any of the labels
+	// if it does then extract the ip address appended after the string (which would look like "app.heimdall.io/10.0.0.1")
+	// this ip address is the source in which requests are allowed to come from
+	// check that the source ip address matches the ip address of the request
+	// if it does then allow the request
+	// if it does not then deny the request
+	// if the string "app.heimdall.io" is not in the key of any of the labels then allow the request
+
+	logrus.Infof("Processing %s request", r.Method)
+
+	// parse the request body into a json object
+	var requestJson map[string]interface{}
+	err := json.NewDecoder(r.Body).Decode(&requestJson)
+	if err != nil {
+		return nil, err
+	}
+	logrus.Info("The request body has been decoded")
+
+	// Convert requestJson["request"].(map[string]interface{})["object"] to unstructured
+	objectJson := requestJson["request"].(map[string]interface{})["object"].(map[string]interface{})
+	unstructuredObject := &unstructured.Unstructured{Object: objectJson}
+
+	if unstructuredObject.GetLabels()["app.heimdall.io/owner"] != "" {
+		owner := unstructuredObject.GetLabels()["app.heimdall.io/owner"]
+		logrus.Infof("owner label: %s", owner)
+		if owner != r.RemoteAddr {
+			logrus.Warnf("owner ip %s does not match request address %s", owner, r.RemoteAddr)
+		} else {
+			logrus.Infof("owner ip %s matches request address %s", owner, r.RemoteAddr)
+		}
+	}
 
 	if r.Method != http.MethodPost {
 		w.WriteHeader(http.StatusMethodNotAllowed)
@@ -62,7 +114,7 @@ func doServeAdmitFunc(w http.ResponseWriter, r *http.Request, admit admitFunc) (
 
 	// Step 2: Parse the AdmissionReview request.
 
-	var admissionReviewReq admission.AdmissionReview
+	var admissionReviewReq v1beta1.AdmissionReview
 
 	if _, _, err := universalDeserializer.Decode(body, nil, &admissionReviewReq); err != nil {
 		w.WriteHeader(http.StatusBadRequest)
@@ -76,12 +128,6 @@ func doServeAdmitFunc(w http.ResponseWriter, r *http.Request, admit admitFunc) (
 
 	admissionReviewResponse := v1beta1.AdmissionReview{
 		Response: &v1beta1.AdmissionResponse{
-			// Since the admission webhook now supports multiple API versions, we need
-			// to explicitly include the API version in the response.
-			// This API version needs to match the version from the request exactly, otherwise
-			// the API server will be unable to process the response.
-			// Note: a v1beta1 AdmissionReview is JSON-compatible with the v1 version, that's why
-			// we do not need to differentiate during unmarshalling or in the actual logic.
 			UID: admissionReviewReq.Request.UID,
 		},
 	}
@@ -109,12 +155,6 @@ func doServeAdmitFunc(w http.ResponseWriter, r *http.Request, admit admitFunc) (
 		}
 		admissionReviewResponse.Response.Allowed = true
 		admissionReviewResponse.Response.Patch = patchBytes
-
-		// Announce that we are returning a JSON patch (note: this is the only
-		// patch type currently supported, but we have to explicitly announce
-		// it nonetheless).
-		admissionReviewResponse.Response.PatchType = new(admission.PatchType)
-		*admissionReviewResponse.Response.PatchType = admission.PatchTypeJSONPatch
 	}
 
 	// Return the AdmissionReview with a response as JSON.
@@ -127,13 +167,29 @@ func doServeAdmitFunc(w http.ResponseWriter, r *http.Request, admit admitFunc) (
 
 // serveAdmitFunc is a wrapper around doServeAdmitFunc that adds error handling and logging.
 func serveAdmitFunc(w http.ResponseWriter, r *http.Request, admit admitFunc) {
-	log.Print("Handling webhook request ...")
-	log.Printf("Here is the raw request method: %v", r.Method)
-	log.Printf("Here is the raw request body: %v", r.Body)
-	log.Printf("Here is the raw request response: %v", r.Response)
-	log.Printf("Here is the raw request method: %v", r.Method)
-	// log ip of source of address
-	log.Printf("Here is the raw request remote address: %v", r.RemoteAddr)
+
+	//// Get the request body from the request
+	//requestBody := r.Body
+	////TODO Will need to convert this to convert this to unstructured so its easier to use
+	////convert requestBody to unstructured
+	//
+	//// parse the request body into a json object
+	//var requestJson map[string]interface{}
+	//json.NewDecoder(requestBody).Decode(&requestJson)
+	//logrus.Infof("Here is the DECODED request body: %v", requestJson)
+	//
+	//// Convert requestJson["request"].(map[string]interface{})["object"] to unstructured
+	//objectJson := requestJson["request"].(map[string]interface{})["object"].(map[string]interface{})
+	//unstructuredObject := &unstructured.Unstructured{Object: objectJson}
+	//
+	//logrus.Infof("Here is the unstructured object: %v", unstructuredObject)
+	//log.Print("Handling webhook request ...")
+	//log.Printf("Here is the raw request method: %v", r.Method)
+	//log.Printf("Here is the raw request body: %v", r.Body)
+	//log.Printf("Here is the raw request response: %v", r.Response)
+	//log.Printf("Here is the raw request method: %v", r.Method)
+	//// log ip of source of address
+	//log.Printf("Here is the raw request remote address: %v", r.RemoteAddr)
 
 	var writeErr error
 	if bytes, err := doServeAdmitFunc(w, r, admit); err != nil {
