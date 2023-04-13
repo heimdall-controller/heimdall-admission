8590f4131b1b833d3667076d634800db826086dc
Bryan Keane <bkeane@redhat.com>
2023-04-13 11:16:29 +0100

    HEIMCTR-61: Changed function for processing requests

--- cmd/admission/main.go
+++ cmd/admission/main.go
@@ -21,7 +21,7 @@ var (
 	podResource = metav1.GroupVersionResource{Version: "v1", Resource: "pods"}
 )
 
-// applySecurityDefaults implements the logic of our example admission controller webhook. For every pod that is created
+// processResourceChanges implements the logic of our example admission controller webhook. For every pod that is created
 // (outside of Kubernetes namespaces), it first checks if `runAsNonRoot` is set. If it is not, it is set to a default
 // value of `false`. Furthermore, if `runAsUser` is not set (and `runAsNonRoot` was not initially set), it defaults
 // `runAsUser` to a value of 1234.
@@ -30,7 +30,7 @@ var (
 // not conflict with the `runAsUser` setting - i.e., if the former is set to `true`, the latter must not be `0`.
 // Note that we combine both the setting of defaults and the check for potential conflicts in one webhook; ideally,
 // the latter would be performed in a validating webhook admission controller.
-func applySecurityDefaults(req *v1beta1.AdmissionRequest, senderIP string, ownerIP string) ([]patchOperation, error) {
+func processResourceChanges(req *v1beta1.AdmissionRequest, senderIP string, ownerIP string) ([]patchOperation, error) {
 	logrus.Infof("triggered admit function for resource %s", req.Name)
 	// check if the ips match, if they do then permit the request
 	if senderIP == ownerIP {
@@ -45,7 +45,7 @@ func main() {
 	keyPath := filepath.Join(tlsDir, tlsKeyFile)
 
 	mux := http.NewServeMux()
-	mux.Handle("/mutate", admitFuncHandler(applySecurityDefaults))
+	mux.Handle("/mutate", admitFuncHandler(processResourceChanges))
 	server := &http.Server{
 		// We listen on port 8443 such that we do not need root privileges or extra capabilities for this server.
 		// The Service object will take care of mapping this port to the HTTPS port 443.
